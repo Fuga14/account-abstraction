ffa78ff9bc75deecdd883d50d389e9cd8393358e
Fuga14 <vladyslavpereder@gmail.com>
2025-01-11 22:02:52 +0200

    feat: added script for deployment abstr acc for ETH

---
+++ script/DeployMinimalAccount.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+
+contract DeployMinimalAccount is Script {
+    function run() public {
+        vm.startBroadcast();
+
+        // Address of sepolia ETH EntryPoint contract
+        address entryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+        MinimalAccount minimalAccount = new MinimalAccount(entryPoint);
+
+        console.log("MinimalAccount deployed at: ", address(minimalAccount));
+
+        vm.stopBroadcast();
+    }
+}
