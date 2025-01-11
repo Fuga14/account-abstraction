69fc15b9ad3bd7e19f39e6ce79d98fc3e2c31281
Fuga14 <vladyslavpereder@gmail.com>
2025-01-11 22:03:21 +0200

    feat: added MinimalAccount abstraction for eth

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,10 +3,102 @@ pragma solidity ^0.8.28;
 
 import {IAccount} from "account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {SIG_VALIDATION_SUCCESS, SIG_VALIDATION_FAILED} from "account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
-contract MinimalAccount is IAccount {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                                STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    IEntryPoint private immutable entryPoint;
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error NonFromEntryPoint();
+
+    error NonFromEntryPointOrOwner();
+
+    error ExecutionFailed(bytes result);
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    modifier onlyEntryPoint() {
+        if (msg.sender != address(entryPoint)) revert NonFromEntryPoint();
+        _;
+    }
+
+    modifier onlyEntryPointOrOwner() {
+        if (msg.sender != address(entryPoint) || msg.sender != owner()) revert NonFromEntryPointOrOwner();
+        _;
+    }
+
+    constructor(address _entryPoint) Ownable(_msgSender()) {
+        entryPoint = IEntryPoint(_entryPoint);
+    }
+
+    receive() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                                EXTERNAL
+    //////////////////////////////////////////////////////////////*/
+
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        onlyEntryPoint
         returns (uint256 validationData)
-    {}
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+
+        //TODO: validate nonce
+        // _validateNonce();
+
+        _payPrefund(missingAccountFunds);
+    }
+
+    function execute(address dest, uint256 value, bytes calldata funcData) external onlyEntryPointOrOwner {
+        // Make a low-level call to dest
+        (bool success, bytes memory ret) = dest.call{value: value}(funcData);
+
+        if (!success) revert ExecutionFailed(ret);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                INTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    // EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        return signer == owner() ? SIG_VALIDATION_SUCCESS : SIG_VALIDATION_FAILED;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function getEntryPoint() external view returns (address) {
+        return address(entryPoint);
+    }
 }
